b34e9cab52850b7d499ff619da9d607d7e9637e0
a105590015 <t105590015@ntut.org.tw>
2018-04-26 01:10:12 +0800

    首頁RWD

--- js/js.js
+++ js/js.js
@@ -5,4 +5,25 @@ $(document).ready(function(){
 	$(".class").mouseleave(function(){
         $(".class_sub").css("visibility", "hidden");
     });
+	
+	$(".announce").mouseover(function(){
+        $(".announce_sub").css("visibility", "visible");
+    });
+	$(".announce").mouseleave(function(){
+        $(".announce_sub").css("visibility", "hidden");
+    });
+	
+	$(".department").mouseover(function(){
+        $(".department_sub").css("visibility", "visible");
+    });
+	$(".department").mouseleave(function(){
+        $(".department_sub").css("visibility", "hidden");
+    });
+	
+	$(".research").mouseover(function(){
+        $(".research_sub").css("visibility", "visible");
+    });
+	$(".research").mouseleave(function(){
+        $(".research_sub").css("visibility", "hidden");
+    });
 });
